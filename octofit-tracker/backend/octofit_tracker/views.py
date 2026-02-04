f3b06a0deda681cb45d1db40c4b7edd9e07d94b2
hebian19 <130063348+hebian19@users.noreply.github.com>
2026-02-04 17:24:58 +0000

    Fix ObjectId serialization in DRF serializers for all models

---
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -0,0 +1,66 @@
+from rest_framework import viewsets
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+from rest_framework import serializers
+
+class TeamSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    def get_id(self, obj):
+        return str(obj.id)
+    class Meta:
+        model = Team
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    team = TeamSerializer(read_only=True)
+    def get_id(self, obj):
+        return str(obj.id)
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class ActivitySerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    user = UserSerializer(read_only=True)
+    def get_id(self, obj):
+        return str(obj.id)
+    class Meta:
+        model = Activity
+        fields = '__all__'
+
+class LeaderboardSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    team = TeamSerializer(read_only=True)
+    def get_id(self, obj):
+        return str(obj.id)
+    class Meta:
+        model = Leaderboard
+        fields = '__all__'
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    def get_id(self, obj):
+        return str(obj.id)
+    class Meta:
+        model = Workout
+        fields = '__all__'
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
+
+class LeaderboardViewSet(viewsets.ModelViewSet):
+    queryset = Leaderboard.objects.all()
+    serializer_class = LeaderboardSerializer
+
+class WorkoutViewSet(viewsets.ModelViewSet):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
